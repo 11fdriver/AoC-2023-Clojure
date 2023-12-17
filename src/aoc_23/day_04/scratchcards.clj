8630aebb514f60230502f7fd72ffdf20579f2ad8
fd <>
2023-12-17 22:34:52 +0000

    Added solutions to day 4 'Scratchcards' for both stars. Also includes a commented early solution, which doesn't work but not sure why.
    FossilOrigin-Name: 3cfe72d4aa23420dd31ce08004f510d750cec79634b5a192f5f1bad1f3eb8242

--- src/aoc_23/day_04/scratchcards.clj
+++ src/aoc_23/day_04/scratchcards.clj
@@ -1,7 +1,78 @@
-(ns aoc-23.day-04.scratchcards)
+(ns aoc-23.day-04.scratchcards
+  (:require [clojure.string :as str]))
 
 (def input-string (slurp "inputs/04"))
 
-(def star-1 nil)
+(defn card-table->vector
+  "Return vector of scratchcards as ([winning numbers] [chosen numbers])."
+  [s]
+  (->> (str/split-lines s)
+       (map #(re-find #"(?<=:).*" %))
+       (mapcat #(str/split % #"\|"))
+       (map str/triml)
+       (map #(str/split % #" +"))
+       (map #(mapv parse-long %))
+       (partition 2)))
 
-(def star-2 nil)
+(defn scratchcard-wins
+  "Return number of wins from a scratchcard."
+  [cards]
+  (->> cards
+       (mapv set)
+       (apply clojure.set/intersection)
+       count))
+
+(defn card-table->points
+  "Return total number of points from table of scratchcards s, with 0 points for
+  no wins or formula '2^(wins-1)'."
+  [s]
+  (let [max-nums (-> (re-find #"(?<=: +).*(?= \|)" input-string)
+                     (str/split #" +")
+                     (count))
+        scores (vec (cons 0 (take max-nums (iterate #(* 2 %) 1))))]
+    (->> s
+         (card-table->vector)
+         (map scratchcard-wins)
+         (map #(get scores %))
+         (reduce +))))
+
+(def star-1 (card-table->points input-string))
+
+(defn card-table->total-cards
+  "Convert table of scratchcards as string s by recursively duplicating the next
+  cards according to the amount of wins on current card, returning total amount
+  of cards encountered."
+  [s]
+  (let [init-cards (card-table->vector s)
+        n-cards (count init-cards)]
+    (loop [[c & cards] init-cards
+           [r & repeats] (repeat n-cards 1)
+           total 0]
+      (if c
+        (let [wins (scratchcard-wins c)]
+          (recur cards (map + repeats (concat (repeat wins r) (repeat 0))) (+ total r))),
+        total))))
+
+(def star-2 (card-table->total-cards input-string))
+
+(comment
+  (defn card-table->points'
+    "Faster way to convert card-table to points than `card-table->points`."
+    [s]
+    (let [max-nums (-> (re-find #"(?<=: +).*(?= \|)" input-string)
+                       (str/split #" +")
+                       (count))
+          scores (vec (cons 0 (take max-nums (iterate #(* 2 %) 1))))
+          xf (comp (map #(re-find #"(?<=:).*" %))
+                   (mapcat #(str/split % #"\|"))
+                   (map str/triml)
+                   (map #(str/split % #" +"))
+                   (map #(mapv parse-long %))
+                   (map #(mapv int %))
+                   (map set)
+                   (completing #(partition 2 %))
+                   ;; Something weird happens around here; don't know why
+                   (map #(apply clojure.set/intersection %))
+                   (map count)
+                   (map #(get scores %)))]
+      (transduce + xf (str/split-lines s)))))
