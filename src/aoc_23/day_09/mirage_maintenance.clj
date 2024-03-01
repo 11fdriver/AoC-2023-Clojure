d1465300f47a668a582849667e2f8fe27d6f90a7
fd <>
2024-03-01 16:14:10 +0000

    Wrote parser for Day 9 'Mirage Maintenance' on top of Creag Chòinnich, Braemar.
    FossilOrigin-Name: e907a9c1cf4fd9b79ffa216840c179036c4c16907e085071a593678047108805

--- src/aoc_23/day_09/mirage_maintenance.clj
+++ src/aoc_23/day_09/mirage_maintenance.clj
@@ -1,7 +1,27 @@
-(ns aoc-23.day-09.mirage-maintenance)
+(ns aoc-23.day-09.mirage-maintenance
+  (:require [clojure.string :as str]))
 
 (def input-string (slurp "inputs/09"))
+
+(def test-input "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45")
+
+(defn parse-oasis-readings
+  [s]
+  (letfn [(split-spaced [s] (str/split s #" "))
+          (parse-longsv [coll] (mapv parse-long coll))]
+    (->> s
+         (str/split-lines)
+         (map split-spaced)
+         (map parse-longsv))))
 
 (def star-1 nil)
 
 (def star-2 nil)
+
+(comment
+  (map (fn [[x y]] (- y x))
+       (partition 2 1
+                  (first (parse-oasis-readings test-input))))
+  )
